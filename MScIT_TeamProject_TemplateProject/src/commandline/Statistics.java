0475465379984ae2a1d8084cc663ea74f0185bd4
stavrinoua <stavrinouadamos@hotmail.com>
2019-01-18 19:04:18 +0000

    Changed some stuff in Statistics

---
+++ MScIT_TeamProject_TemplateProject/src/commandline/Statistics.java
@@ -0,0 +1,60 @@
+package commandline;
+
+public class Statistics {
+
+	private int numberOfGamesPlayed(int a, int b) {
+		int totalGamesPlayed = 0;
+		/*
+		 * ADD CODE HERE
+		 */
+		return totalGamesPlayed;
+	}
+	
+	private int numberOfTimesAIWon() {
+		int totalTimesAIWon = 0;
+		/*
+		 * ADD CODE HERE
+		 */
+		return totalTimesAIWon;
+	}
+	
+	private int numberOfTimesHumanWon() {
+		int totalPlayerWins = 0;
+		/*
+		 * ADD CODE HERE
+		 */
+		return totalPlayerWins;
+	}
+	
+	private double averageNumberOfDraws() {
+		double avgDraws = 0.0;
+		/*
+		 * ADD CODE HERE
+		 */
+		return Math.round(avgDraws);
+	}
+	
+	private int largestNumberOfRoundsPlayed() {
+		int maxRoundsPlayed = 0;
+		/*
+		 * ADD CODE HERE
+		 */
+		return maxRoundsPlayed;
+	}
+	
+	private int numberOfDrawsInAGame() {
+		int drawsInGame = 0;
+		/*
+		 * ADD CODE HERE
+		 */
+		return drawsInGame;
+	}
+	
+	private void numberOfRoundsPlayedInAGame() {
+		int maxRoundsInGame = 0;
+		/*
+		 * ADD CODE HERE
+		 */
+		return maxRoundsInGame;
+	}
+}
