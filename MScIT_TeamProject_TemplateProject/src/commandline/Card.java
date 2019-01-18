bc19b3cd179dbf005b86c57c03b1a97f88c59a78
mogyi5 <szaboki.reka@gmail.com>
2019-01-18 10:51:28 +0000

    Added a class for cards
    Added Card(name, attributes)

---
+++ MScIT_TeamProject_TemplateProject/src/commandline/Card.java
@@ -0,0 +1,21 @@
+package commandline;
+
+public class Card {
+	
+	String cardName;
+	int[] attributeValues;
+	
+	public Card(String n, int[] v) {
+		cardName = n;
+		attributeValues = v;
+	}
+	
+	public String getCardName() {
+		return cardName;
+	}
+	
+	public int[] getAttributeValues() {
+		return attributeValues;
+	}
+
+}
