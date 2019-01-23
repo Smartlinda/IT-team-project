24e1ec58921cb5dca18b7a4c1b1eef4dfa5f8585
stavrinoua <stavrinouadamos@hotmail.com>
2019-01-23 16:41:57 +0000

    Changes to DBConnect, statistics and main

--- MScIT_TeamProject_TemplateProject/src/commandline/Statistics.java
+++ MScIT_TeamProject_TemplateProject/src/commandline/Statistics.java
@@ -1,15 +1,66 @@
 package commandline;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Statistics {
 
-	private int numberOfGamesPlayed(int a, int b) {
+	int gamesPlayed;
+	
+	DBConnect db = new DBConnect();
+
+	public Statistics() {
+
+	}// End of constructor
+
+	private void updateValuesInDB() {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (Exception e) {
+			System.out.println("Could not load class " + "org.postgresql.Drive");
+			e.printStackTrace();
+		} // try-catch exception
+		// the driver is loaded...
+		System.out.println("PostgreSQL JDBC Driver found!");
+		// proceed with a database connection
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "teamproject");
+		} catch (SQLException e) {
+			System.out.println("Connection failed!");
+			e.printStackTrace();
+		}
+		
+		// connection to the database is done
+		if (connection != null) {
+		try {
+			System.out.println("Established connection to database.\n");
+			Statement statement = connection.createStatement();
+			String sql = "UPDATE games_stats SET games_played = " + gamesPlayed;
+			ResultSet rs = statement.executeQuery(sql);
+			
+			// do not forget to close the connection to the database
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} // try-catch exception
+		} else {
+			System.out.println("Failed to establish connection!");
+		} // if-else
+
+	}// End of method
+
+	private void numberOfGamesPlayed(int a, int b) {
 		int totalGamesPlayed = 0;
 		/*
 		 * ADD CODE HERE
 		 */
-		return totalGamesPlayed;
+		totalGamesPlayed = gamesPlayed;
 	}
-	
+
 	private int numberOfTimesAIWon() {
 		int totalTimesAIWon = 0;
 		/*
@@ -17,7 +68,7 @@ public class Statistics {
 		 */
 		return totalTimesAIWon;
 	}
-	
+
 	private int numberOfTimesHumanWon() {
 		int totalPlayerWins = 0;
 		/*
@@ -25,7 +76,7 @@ public class Statistics {
 		 */
 		return totalPlayerWins;
 	}
-	
+
 	private double averageNumberOfDraws() {
 		double avgDraws = 0.0;
 		/*
@@ -33,7 +84,7 @@ public class Statistics {
 		 */
 		return Math.round(avgDraws);
 	}
-	
+
 	private int largestNumberOfRoundsPlayed() {
 		int maxRoundsPlayed = 0;
 		/*
@@ -41,7 +92,7 @@ public class Statistics {
 		 */
 		return maxRoundsPlayed;
 	}
-	
+
 	private int numberOfDrawsInAGame() {
 		int drawsInGame = 0;
 		/*
@@ -49,7 +100,7 @@ public class Statistics {
 		 */
 		return drawsInGame;
 	}
-	
+
 	private int numberOfRoundsPlayedInAGame() {
 		int maxRoundsInGame = 0;
 		/*
