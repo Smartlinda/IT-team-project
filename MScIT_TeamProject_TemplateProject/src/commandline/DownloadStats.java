0187e3c5331da8b393e753b0a8ca7fbed99583e2
stavrinoua <stavrinouadamos@hotmail.com>
2019-02-01 20:33:31 +0000

    changes

--- MScIT_TeamProject_TemplateProject/src/commandline/DownloadStats.java
+++ MScIT_TeamProject_TemplateProject/src/commandline/DownloadStats.java
@@ -13,30 +13,18 @@ public class DownloadStats {
 	String winner;
 	int numberOfDraws;
 
-	// proceed with a database connection
 	Connection connection = null;
-	ResultSet rs0 = null;
-	ResultSet rs1 = null;
-	ResultSet rs2 = null;
-	ResultSet rs3 = null;
-	ResultSet rs4 = null;
-	
-	
-	
-	
-	
+
 	Statement smt = null;
 
-	
-	String[] queries = {
-			"SELECT COUNT(game_id) FROM games_stats", 
+	String[] queries = { "SELECT COUNT(game_id) FROM games_stats",
 			"SELECT COUNT(winner) FROM games_stats WHERE winner='AI'",
 			"SELECT COUNT(winner) FROM games_stats WHERE winner='Human'",
-			"SELECT ROUND(AVG(number_of_draws),2) FROM games_stats", 
-			"SELECT MAX(number_of_rounds) FROM games_stats", 
-			};
-	
-	
+			"SELECT ROUND(AVG(number_of_draws),2) FROM games_stats", "SELECT MAX(number_of_rounds) FROM games_stats", };
+
+	ResultSet rs0 = null;
+	Number[] query = new Number[queries.length];
+
 	/*
 	 * Constructor
 	 */
@@ -69,6 +57,7 @@ public class DownloadStats {
 			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres",
 					"teamproject");
 
+			// change to this when we running the game from the labs
 //			connection = DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/", "m_18_2416090c",
 //					"2416090c");
 			System.out.println("Connection Established.");
@@ -85,98 +74,38 @@ public class DownloadStats {
 	private void getTableStats() {
 		try {
 			smt = connection.createStatement();
-			connection.setAutoCommit(false);
+
 			for (int i = 0; i < queries.length; i++) {
-				smt.addBatch(queries[i]);
+				rs0 = smt.executeQuery(queries[i]);
+				rs0.next();
+				query[i] = ((Number) rs0.getObject(1)).intValue();
 			}
+
 		} catch (SQLException e) {
 			System.out.println("Oups. That's embarrassing. Something went wrong...");
 			e.printStackTrace();
-		} // End of try-catch
+		}
 
-		// query to get all records from table postgres
-
-		try {
-				rs0 = smt.executeQuery(queries[0]);
-//				rs1 = smt.executeQuery(queries[1]);
-				
-		} catch (SQLException e) {
-			System.out.println("Oups. That's embarrassing. Something went wrong...");
-			e.printStackTrace();
-		} // End of try-catch
 		showTableStats();
 	}// End of method
 
 	/*
 	 * This method will show the table with the statistics of the player
 	 */
-	private void showTableStats() {	
-				int i = 0;
-		try {
-			if (rs0.next()) {
-				String leftAlignFormat = "| %-11s | %-16s | %-10s | %-15s |%n";
+	private void showTableStats() {
+		int i = 0;
+		String leftAlignFormat = "| %-24s | %-12s | %-19s | %-19s | %-36s |%n";
 
-				System.out.format("+-------------+------------------+------------+-----------------+%n");
-				System.out.format("|   Game ID   | Number of Rounds |   Winner   | Number of Draws |%n");
-				System.out.format("+-------------+------------------+------------+-----------------+%n");
+		System.out.format(
+				"+--------------------------+--------------+---------------------+---------------------+--------------------------------------+%n");
+		System.out.format(
+				"|   Games Played Overall   | AI Times Won |   Human Times Won   | AVG Number of Draws | Highest Amount of Rounds in One Game |%n");
+		System.out.format(
+				"+--------------------------+--------------+---------------------+---------------------+--------------------------------------+%n");
 
-				// Add the values within the table and also add the values to a global value
-				do {
-					game_id = rs0.getInt(1);
-//					numberOfRounds = rs1.getInt(queries[1]);
-//					winner = rs2.getString(queries[2]);
-//					numberOfDraws = rs3.getInt(queries[3]);
-
-					System.out.format(leftAlignFormat, game_id, numberOfRounds, winner, numberOfDraws);
-					System.out.format("+-------------+------------------+------------+-----------------+%n");
-//a sasd
-				} while (rs0.next() && rs1.next());
-
-			} else {
-				System.out.println("Record Not Found...");
-			} // End of if else
-		} catch (SQLException e) {
-			System.out.println("Oups! Something went wrong!");
-			e.printStackTrace();
-		} // End of try-catch
+		System.out.format(leftAlignFormat, query[0], query[1], query[2], query[3], query[4]);
+		System.out.format(
+				"+--------------------------+--------------+---------------------+---------------------+--------------------------------------+%n");
 	}// End of method
 
-	/*
-	 * GETTERS AND SETTERS - START
-	 */
-	public int getGameID() {
-		return game_id;
-	}
-
-	public void setGameID(int game_id) {
-		this.game_id = game_id;
-	}
-
-	public int getNumberOfRounds() {
-		return numberOfRounds;
-	}
-
-	public void setNumberOfRounds(int numberOfRounds) {
-		this.numberOfRounds = numberOfRounds;
-	}
-
-	public String getWinner() {
-		return winner;
-	}
-
-	public void setWinner(String winner) {
-		this.winner = winner;
-	}
-
-	public int getNumberOfDraws() {
-		return numberOfDraws;
-	}
-
-	public void setNumberOfDraws(int numberOfDraws) {
-		this.numberOfDraws = numberOfDraws;
-	}
-
-	/*
-	 * GETTERS AND SETTERS - END
-	 */
 }// End of class
